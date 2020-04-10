27343ba2c4b641dadaca54d49e86c67b777e6bf5
Tommaso Caneva <tommaso.caneva@glovoapp.com>
2020-04-10 21:10:50 +0200

    adding ls

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,4 +2,5 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 echo | pwd
+echo | ls
 unzip -o packages/regression_model/datasets/house-prices-advanced-regression-techniques.zip -d packages/regression_model/datasets
